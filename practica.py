2a4facbfb9bdd8e3750f5375afd4ca209e82b0db
yorman-blib <vyorman72@gmail.com>
2025-04-20 08:11:25 -0500

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,30 @@
+<html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Photo Gallery</title>
+	<link rel="stylesheet" href="css/gallery.css">
+	<script src = "js/gallery.js"></script>
+</head>
+<body>
+	
+	<div id = "image">
+		Hover over an image below to display here.
+	</div>
+	
+	<img class = "preview" alt = "Styling with a Bandana" src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon.jpg" onmouseover = "upDate(this)" onmouseout = "unDo()">
+	
+	<img class = "preview" alt = "With My Boy" src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon2.JPG" onmouseover = "upDate(this)" onmouseout = "unDo()">
+	
+	<img class = "preview" src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon3.jpg" alt = "Young Puppy" onmouseover = "upDate(this)" onmouseout = "unDo()">
+  
+  	<img class = "preview" src = "https://www.clinicas-veterpet.com/wp-content/uploads/2022/07/blog_fox_terrier_veterpet.jpg" alt = "imágen de terrier" onmouseover = "upDate(this)" onmouseout = "unDo()">
+  
+  <img class = "preview" src = "https://t1.ea.ltmcdn.com/es/posts/9/2/8/galletas_para_perros_caseras_5_recetas_faciles_24829_600.jpg"  alt = "perros" onmouseover = "upDate(this)" onmouseout = "unDo()">
+  
+  <img class = "preview" src = "https://okdiario.com/img/2023/05/04/el-mejor-truco-casero-para-quitar-las-garrapatas-del-perro-635x358.jpg"  alt = "perros" onmouseover = "upDate(this)" onmouseout = "unDo()">
+  
+ 
+
+</body>
+</html>
